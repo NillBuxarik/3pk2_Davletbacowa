a9c04b80a197e56dcad7f22f630dfbed017388f3
Нелли Давлетбакова <nellidavletbakova.2017@gmail.com>
2023-03-03 23:56:19 +0500

    3

---
+++ PZ-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BynaryTree tree = new BynaryTree(6);
+
+            //BynaryTree.GetTreeData(tree.Root);
+            Console.Write("Среднее значение:");
+            BynaryTree.AverageArif(tree.Root);
+            Console.WriteLine("Положительные числа " + BynaryTree.CountPlus(tree.Root));
+            Console.WriteLine("Отрицательные числа " + BynaryTree.CountMinus(tree.Root));
+
+            Console.Write("Введите число для поиска: ");
+            int sverka = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество вхождений вашего числа: " + BynaryTree.Countsverka(tree.Root, sverka));
+            Console.ReadKey();
+        }
+    }
+}
